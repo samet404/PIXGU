4458a94fd2466cc304c1c06ee47d3dde3cf5fab2
404 <145053429+404nnotfoundddd@users.noreply.github.com>
2023-11-02 18:19:23 +0300

    18:19
    npm run build

--- src/app/game/_components/UsersSelection/User.tsx
+++ src/app/game/_components/UsersSelection/User.tsx
@@ -1,21 +1,21 @@
-import { NextFont } from '@next/font'
+import type { NextFont } from 'next/font'
 import { Pixelify_Sans, Tilt_Neon } from 'next/font/google'
 import Image from 'next/image'
 import pfp from '@/png/pfp2.png'
 
 const pixelifySans: NextFont = Pixelify_Sans({
-    subsets: ['latin'],
-    weight: '700',
+  subsets: ['latin'],
+  weight: '700',
 })
 
 const tiltNeon = Tilt_Neon({ subsets: ['latin'] })
 
-type User = {
+type UserProps = {
     name: string
     score: number
 }
 
-const User = ({ name, score }: User) => {
+const User = ({ name, score }: UserProps) => {
     return (
         <div className="flex flex-row items-center justify-between rounded-md bg-[rgba(255,255,255,0.4)] p-2">
             <div className="flex flex-row items-center gap-2 ">
