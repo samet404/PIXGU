d0443f0121e97101a8fe73e4838ca739383eff74
404 <145053429+404nnotfoundddd@users.noreply.github.com>
2023-11-02 20:01:15 +0300

    11/02/2023 20:01:15

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -58,30 +58,34 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`
+    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`
     const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+         FROM invoices`
 
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
-    ]);
+    ])
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    // @ts-expect-error u
+    const numberOfInvoices = Number(data[0].rows[0].count ?? '0')
+    // @ts-expect-error u
+    const numberOfCustomers = Number(data[1].rows[0].count ?? '0')
+    // @ts-expect-error u
+    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0')
+    // @ts-expect-error u
+    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0')
 
     return {
       numberOfCustomers,
       numberOfInvoices,
       totalPaidInvoices,
       totalPendingInvoices,
-    };
+    }
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to card data.');
@@ -135,10 +139,10 @@ export async function fetchInvoicesPages(query: string) {
       invoices.amount::text ILIKE ${`%${query}%`} OR
       invoices.date::text ILIKE ${`%${query}%`} OR
       invoices.status ILIKE ${`%${query}%`}
-  `;
-
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-    return totalPages;
+  `
+    // @ts-expect-error u
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE)
+    return totalPages
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch total number of invoices.');
