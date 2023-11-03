99216c1be98b5933dbaaab381bebdfcdbb700457
404 <145053429+404nnotfoundddd@users.noreply.github.com>
2023-11-03 22:56:24 +0300

    11/03/2023 22:56:24

--- src/app/support/[[...slug]]/page.tsx
+++ src/app/support/[[...slug]]/page.tsx
@@ -1,14 +1,19 @@
-const page = ({ params }: { params: { slug: string[] } }) => {
+import { Fragment } from 'react'
 
-    setTimeout(() => console.log(params.slug), 3000)
+const Support = ({ params }: { params: { slug: string[] } }) => {
+  setTimeout(() => console.log(params.slug), 3000)
 
-    return (
-        <>
-            {params.slug.map((slug, index) => (
-                <div key={index}>{slug}</div>
-            ))}
-        </>
-    )
+  const content = () => {
+    if (params.slug) {
+        return params.slug.map((slug, index) => <div key={index}>{slug}</div>)
+    }
+    else {
+        return <div>slug yok</div>
+    }
+  }
+  return <Fragment>
+    {content()}
+  </Fragment>
 }
 
-export default page
+export default Support
