752f071744bfb0ad580eba02fcf625ab7540ae0b
404 <sametalpdeger9@gmail.com>
2024-12-13 19:22:49 +0300

    Update simplePeer.ts

--- src/utils/simplePeer.ts
+++ src/utils/simplePeer.ts
@@ -5,21 +5,6 @@ export const iceServers: RTCIceServer[] = [
     urls: "stun:stun.relay.metered.ca:80",
   },
   {
-    urls: "turn:global.relay.metered.ca:80",
-    username: "e0bc174ce6b35fe4b4e3bab6",
-    credential: "W9oGhBvOcEPA9vP7",
-  },
-  {
-    urls: "turn:global.relay.metered.ca:80?transport=tcp",
-    username: "e0bc174ce6b35fe4b4e3bab6",
-    credential: "W9oGhBvOcEPA9vP7",
-  },
-  {
-    urls: "turn:global.relay.metered.ca:443",
-    username: "e0bc174ce6b35fe4b4e3bab6",
-    credential: "W9oGhBvOcEPA9vP7",
-  },
-  {
     urls: "turns:global.relay.metered.ca:443?transport=tcp",
     username: "e0bc174ce6b35fe4b4e3bab6",
     credential: "W9oGhBvOcEPA9vP7",
@@ -34,6 +19,7 @@ export const simplePeer = (opts?: SimplePeer.Options): SimplePeer.Instance => {
   const iceServersData = opts?.config?.iceServers ?? iceServers
   if (!iceServersData || iceServersData.length === 0)
     throw new Error('No iceServers provided')
+
 
   return new SimplePeer({
     initiator: opts?.initiator ?? false,
