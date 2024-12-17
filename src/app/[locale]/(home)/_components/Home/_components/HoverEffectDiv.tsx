bd1b1c02d7ff110d5dfc663df0c924e3268b67e7
404 <sametalpdeger9@gmail.com>
2024-12-18 01:07:51 +0300

    xata issue

--- src/app/[locale]/(home)/_components/Home/_components/HoverEffectDiv.tsx
+++ src/app/[locale]/(home)/_components/Home/_components/HoverEffectDiv.tsx
@@ -3,7 +3,6 @@
 import { type ReactNode } from 'react'
 
 const HoverEffectDiv = ({ children }: { children: ReactNode }) => {
-  console.log('HoverEffectDiv rendered')
   return (
     <div className="flex h-full w-full flex-col items-center gap-3 overflow-y-auto duration-500  ">
       {children}
--- src/server/db/initilizeXata.ts
+++ src/server/db/initilizeXata.ts
@@ -1,8 +1,7 @@
 import { env } from '@/env/server'
 
-console.log('Xata database is initilizing...')
-
 const isCanary = env.CANARY === '1'
-Bun.spawn(["bun", "run", isCanary ? 'xata:init:canary' : 'xata:init'])
-
-console.log('Xata database is initilized!')
+const apiKey = env.XATA_API_KEY
+Bun.spawn(['bun', 'add', '@xata.io/client@next'])
+Bun.spawn(['xata', 'auth', 'login', `--api-key=${apiKey}`, '--force', '--no-input'])
+Bun.spawn(['bun', 'run', isCanary ? 'xata:init:canary' : 'xata:init'])
