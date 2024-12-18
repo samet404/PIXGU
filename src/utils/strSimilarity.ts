954c201e90cc19c2f39d2410f97f400661207759
404 <sametalpdeger9@gmail.com>
2024-12-18 21:13:20 +0300

    Update strSimilarity.ts

--- src/utils/strSimilarity.ts
+++ src/utils/strSimilarity.ts
@@ -4,25 +4,25 @@ const editDistance = (str1: string, str2: string): number => {
 
     const costs = [];
     for (let i = 0; i <= str1.length; i++) {
-        let lastValue = i;
+        let lastValue = i
         for (let j = 0; j <= str2.length; j++) {
             if (i == 0)
                 costs[j] = j;
             else {
                 if (j > 0) {
-                    let newValue = costs[j - 1];
+                    let newValue = costs[j - 1]
                     if (str1.charAt(i - 1) != str2.charAt(j - 1))
-                        newValue = Math.min(Math.min(newValue, lastValue),
-                            costs[j]) + 1;
-                    costs[j - 1] = lastValue;
-                    lastValue = newValue;
+                        newValue = Math.min(Math.min(newValue!, lastValue),
+                            costs[j]!) + 1
+                    costs[j - 1] = lastValue
+                    lastValue = newValue!
                 }
             }
         }
         if (i > 0)
-            costs[str2.length] = lastValue;
+            costs[str2.length] = lastValue
     }
-    return costs[str2.length];
+    return costs[str2.length]!
 }
 
 /**
