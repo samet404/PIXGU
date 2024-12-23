6b190576768ba43ac9f80e43260515e5fa10fd89
404 <sametalpdeger9@gmail.com>
2024-12-23 20:46:40 +0300

    ip

--- src/utils/getIP.ts
+++ src/utils/getIP.ts
@@ -1,8 +1,8 @@
 export const getIP = (headers: Headers) => {
     const FALLBACK_IP_ADDRESS = '0.0.0.0'
-    const forwardedFor = headers.get('x-forwarded-for')
+    const forwardedFor = headers.get('cf-connecting-ip')
 
     if (forwardedFor) return forwardedFor.split(',')[0] ?? FALLBACK_IP_ADDRESS
 
-    return headers.get('x-real-ip') ?? FALLBACK_IP_ADDRESS
+    return headers.get('x-forwarded-for') ?? FALLBACK_IP_ADDRESS
 }
