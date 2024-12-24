4cafc93bbef8d81268c1d2e7a766a65ab56e541f
404 <sametalpdeger9@gmail.com>
2024-12-24 16:17:02 +0300

    Update playerLeftFromSocket.ts

--- src/app/[locale]/r/[roomID]/h/components/Joined/components/ConnectToPeers/funcs/playerLeftFromSocket.ts
+++ src/app/[locale]/r/[roomID]/h/components/Joined/components/ConnectToPeers/funcs/playerLeftFromSocket.ts
@@ -1,14 +1,11 @@
 import { negativeLog } from '@/utils'
 import { useSocketIO } from '@/zustand/store'
 import { playerLeft } from './playerLeft'
-import type { Guest } from '@/types/guest'
-import type { User } from 'lucia'
 
 export const playerLeftFromSocket = (roomID: string) =>
   useSocketIO
     .getState()
-    .io!.on('player-left', (user: Guest | User) => {
-      const ID = 'ID' in user ? user.ID : user.id
+    .io!.on('player-left', (ID: string) => {
       negativeLog(`USER ${ID} LEFT FROM SOCKET`)
 
       playerLeft(ID, roomID)
